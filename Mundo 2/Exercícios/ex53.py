f3169409c8baaddad5de315a0f14284b22f12782
luisastellet <luisamstellet@gmail.com>
2023-04-02 15:53:04 -0300

    .

--- Mundo 2/Exercícios/ex53.py
+++ Mundo 2/Exercícios/ex53.py
@@ -1,4 +1,11 @@
-frase = str(input('Digite uma frase: ')).strip().upper()
-palavras = frase.split()
+frase = str(input('Digite uma frase: ')).strip().upper() # .strip() faz tirar os espaços de antes e depois da frase e .upper() deixa tudo maiúsculo 
+palavras = frase.split() # split() e join() separam em termos e juntam eles, eliminando espaços internos
 junto = ''.join(palavras)
-print('Você digitou a frase {}' .format(junto))
+inverso = ''
+for letra in range (len(junto) - 1, -1, -1):
+    inverso += junto[letra]
+print('Você digitou a frase {} que ao contrário fica {}' .format(junto, inverso))
+if inverso == junto:
+    print('Temos um palíndromo!!!')
+else:
+    print('A frase digitada NÃO é um palíndromo!!!')
