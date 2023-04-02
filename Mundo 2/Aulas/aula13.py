c580e5554357979f524ded246472f976b47eb32b
luisastellet <luisamstellet@gmail.com>
2023-04-02 10:22:43 -0300

    .

---
+++ Mundo 2/Aulas/aula13.py
@@ -0,0 +1,21 @@
+#n = int(input('Digite um número: '))
+#for c in range(0, n+1):
+    #print(c)
+#print('FIM')
+
+
+
+#i = int(input('Início: '))
+#f = int(input('Fim: '))
+#p = int(input('Passo: '))
+#for c in range (i, f+1, p):
+    #print(c)
+#print('FIM')
+
+
+
+s = 0
+for c in range (0, 4):
+    n = int(input('Digite um valor: '))
+    s = s + n
+print('O somatório de todos os valores foi {}' .format(s))
