07b3f47287304dafe4a5b052c084229d52ffb86a
luisastellet <luisastellet@id.uff.br>
2023-04-07 11:51:42 -0300

    .

--- Mundo 2/Exercícios/ex57.py
+++ Mundo 2/Exercícios/ex57.py
@@ -1,4 +1,4 @@
-sexo = 'S'
+sexo = str(input('Digite o seu sexo [M ou F]: ')).upper()
 while sexo != 'M' and sexo != 'F':
-    sexo = str(input('Digite o seu sexo [M ou F]: '))
+    sexo = str(input('Dados inválidos, digite novamente [M ou F]: ')).upper()
 print('Obrigada pela resposta!')
--- Mundo 2/Exercícios/ex58.py
+++ Mundo 2/Exercícios/ex58.py
@@ -4,7 +4,12 @@ comp = random.choice(n)
 
 user = 1
 palpite = 0
+print(comp)
 while user != comp:
     user = int(input('Chute qual número foi sorteado: '))
     palpite += 1
+    if user == comp + 1 or user == comp + 2:
+        print('=== Desce um pouco esse valor ===')
+    elif user == comp - 1 or user == comp - 2:
+        print('=== Sobe um pouco esse valor ===')
 print('PARABÉNSSSS! VOCÊ ACERTOU! Você precisou de {} palpites para acertar.' .format(palpite))
